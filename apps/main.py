22f0d3592c2cbe6baeef59b69debc77251a6be7e
hafizurfpbd <hafizurfpbd@gmail.com>
2026-04-17 01:06:25 +0600

    link add

--- apps/main.py
+++ apps/main.py
@@ -2,6 +2,7 @@
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 from pathlib import Path
 from typing import Optional, List
 
@@ -12,6 +13,19 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 templates.env.cache_size = 0  # cache error fix
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request, name: Optional[str] = None):
+    return templates.TemplateResponse(request=request, name="dashboard.html",context={"name":name})
+
+@app.get("/dashboard", response_class=HTMLResponse)
+async def home(request: Request, name: Optional[str] = None):
+    return templates.TemplateResponse(request=request, name="dashboard.html",context={"name":name})
+
+@app.get("/dataupload", response_class=HTMLResponse, name="dataupload")
+async def home(request: Request, name: Optional[str] = None):
+    return templates.TemplateResponse(request=request, name="dashboard.html",context={"name":name})
+
+
+@app.get("/dataprofiling", response_class=HTMLResponse, name="dataprofiling")
 async def home(request: Request, name: Optional[str] = None):
     return templates.TemplateResponse(request=request, name="dashboard.html",context={"name":name})
